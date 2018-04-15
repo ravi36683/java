6803e4078a3cbfe31f470e63458bf8333ca17e33
Ravi Gupta <ravi36683@gmail.com>
2018-04-15 13:43:10 +0530

    Update Patern.java

--- java_-tutorials/src/Patern.java
+++ java_-tutorials/src/Patern.java
@@ -28,7 +28,7 @@ public class Patern {
 				if(i==1||i==4||j==1||j==4)
 				System.out.print("$");
 				else
-					System.out.print("");
+					System.out.print(" ");
 			}
 			System.out.print("\n");
 		}
